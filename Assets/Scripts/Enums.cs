19e22d6933ae23f29edbf556e2ffdb8b0ba4b10a
Dwelwin <157803725+Dwelwin@users.noreply.github.com>
2024-01-27 15:10:54 +0200

    Lista erikoisainesosista

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -8,7 +8,21 @@ public enum IngredientType
     //Wholesale 100-199
     Sausage = 100,
     Steak = 101,
+    Soymilk = 102,
+    WhitefishTwoKilograms = 103,
         
     //Sawmill 200-299
     Sawdust = 200,
+    MotorOil = 201,
+    HandCream = 202,
+
+    //"Apothecary" 300-399
+    NobleSpirit = 301,
+    PainKillers = 302,
+    MagicMushroom = 303,
+
+    //Hell Shop 400-499
+    Soul = 401,
+    VirginBlood = 402,
+    MysteryMeat = 403,
 }
