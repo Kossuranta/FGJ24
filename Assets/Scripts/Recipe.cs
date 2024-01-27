5abd2674d5f4bcbb743ac2177b5e4cebe8477cbf
Kossuranta <kossuranta@gmail.com>
2024-01-27 02:51:24 +0200

    Handle recipe moving and reordering

--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -1,24 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Recipe : MonoBehaviour, IDragHandler
+public class Recipe : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
+    [NonSerialized]
+    public RectTransform m_rect;
+    
+    private RecipeManager m_recipeManager;
     private Canvas m_canvas;
-    private RectTransform m_rect;
+    private Vector2 m_offset;
 
-    public void Initialize(Canvas _canvas)
+    public void Initialize(RecipeManager _recipeManager, Canvas _canvas)
     {
+        m_recipeManager = _recipeManager;
         m_canvas = _canvas;
         m_rect = (RectTransform)transform;
     }
     
+    public void OnPointerDown(PointerEventData _eventData)
+    {
+        m_recipeManager.OnRecipeDragStart(this);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(m_rect, _eventData.position, m_canvas.worldCamera, out m_offset);
+    }
+
     public void OnDrag(PointerEventData _eventData)
     {
         Vector2 screenPos = Input.mousePosition;
-        float widthMultiplier = 1920f / Screen.width;
-        float heightMultiplier = 1080f / Screen.height;
+        float widthMultiplier = (float)GameManager.SCREEN_WIDTH / Screen.width;
+        float heightMultiplier = (float)GameManager.SCREEN_HEIGHT / Screen.height;
         float xPos = screenPos.x * widthMultiplier;
-        float yPos = screenPos.y * heightMultiplier;
-        m_rect.anchoredPosition = new Vector2(xPos, yPos);
+        float yPos = screenPos.y * heightMultiplier - 128;
+        m_rect.anchoredPosition = new Vector2(xPos, yPos) - m_offset;
     }
 }
--- Assets/Scripts/RecipeManager.cs
+++ Assets/Scripts/RecipeManager.cs
@@ -14,11 +14,17 @@ public class RecipeManager : MonoBehaviour
 
     private void GenerateRecipes()
     {
-        Recipe r = Instantiate(m_recipePrefab, transform);
-        m_recipes.Add(r);
-        foreach (Recipe recipe in m_recipes)
+        for (int i = 0; i < 10; i++)
         {
-            recipe.Initialize(m_canvas);
+            Recipe r = Instantiate(m_recipePrefab, transform);
+            m_recipes.Add(r);
+            r.Initialize(this, m_canvas);
+            r.m_rect.anchoredPosition = new Vector2(400 + (i*20), 200 - (i*20));
         }
+    }
+
+    public void OnRecipeDragStart(Recipe _recipe)
+    {
+        _recipe.transform.SetSiblingIndex(m_recipes.Count);
     }
 }
